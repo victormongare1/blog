0501f4ba1fd6886bc63b62e8427b65c67ee2e035
victormongare1 <vicmongz254@gmail.com>
2020-05-09 11:17:51 +0300

    added view for logout

--- app/auth/views.py
+++ app/auth/views.py
@@ -28,3 +28,8 @@ def register():
     return redirect(url_for('auth.login'))
     title="New Account"
   return render_template('auth/register.html',registration_form=form)    
+@auth.route('/logout')
+@login_required
+def logout():
+  logout_user()
+  return redirect(url_for("main.index"))      
