3fd33595212fecebe2654d1ce3dfcbd242ed77a8
victormongare1 <vicmongz254@gmail.com>
2020-05-09 11:16:52 +0300

    added view for registering

--- app/auth/views.py
+++ app/auth/views.py
@@ -14,5 +14,17 @@ def login():
       login_user(user,login_form.remember.data)
       return redirect(request.args.get('next') or url_for('main.index'))
     flash('Invalid username or Password')
-  title = "Pitch login"
+  title = "Blog login"
   return render_template('auth/login.html',login_form = login_form,title=title)  
+@auth.route('/register',methods=["GET","POST"])
+def register():
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    user = User(email=form.email.data,username=form.username.data,password=form.password.data) 
+    db.session.add(user)
+    db.session.commit()
+    mail_message("Welcome to Pitch","email/welcome_user",user.email,user=user)
+
+    return redirect(url_for('auth.login'))
+    title="New Account"
+  return render_template('auth/register.html',registration_form=form)    
