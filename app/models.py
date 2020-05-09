4b0d4972e30302333cd23b3590aaa622c3bf7308
victormongare1 <vicmongz254@gmail.com>
2020-05-09 11:26:22 +0300

    created user class

--- app/models.py
+++ app/models.py
@@ -6,3 +6,15 @@ from flask_login import UserMixin
 @login_manager.user_loader
 def load_user(user_id):
   return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
+  '''
+  user class to define user objects
+  '''
+  __tablename__='users'
+
+  id=db.Column(db.Integer,primary_key = True)
+  username=db.Column(db.String(255),index=True)
+  email=db.Column(db.String(255),unique=True,index=True)
+  pass_secure=db.Column(db.String(255))
+  password_hash=db.Column(db.String(255))
